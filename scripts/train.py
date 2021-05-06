5ab9bcf00f592a51b27067d0f7d95ecd21f785f4
Sarthak Jain <jainsarthak101@gmail.com>
2021-05-05 17:09:46 -0700

    Add files via upload

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,9 @@ import fruit_dataframe
 import warnings
 import torch
 
+#git clone this repo from https://github.com/lessw2020/Ranger-Deep-Learning-Optimizer
+from ranger import Ranger
+
 
 def create_dataloaders(prep_noiseloader = True, train_batch_size = 2, test_batch_size = 2):
 
@@ -12,7 +15,7 @@ def create_dataloaders(prep_noiseloader = True, train_batch_size = 2, test_batch
         noise_loader = torch.utils.data.DataLoader(noise_dataset, batch_size = test_batch_size, shuffle = True)
 
     else:
-        warnings.warn("Not loading with noise loader, hence Model may generalize worse")
+        warnings.warn("Not loading with noise loader, hence built in Model function will error")
         noise_path = None
 
     bounding_box_dict, labels_dict = fruit_dataframe.get_dict(["Placeholder", "Apples", "Strawberry", "Apple_Bad_Spot", "Strawberry_Bad_Spot"])
@@ -33,5 +36,65 @@ def create_dataloaders(prep_noiseloader = True, train_batch_size = 2, test_batch
 def collate_fn(batch):
   return tuple([list(a) for a in zip(*batch)])
 
+''' Foret, Pierre, et al. “Sharpness-Aware Minimization for Efficiently Improving Generalization.”
+ArXiv.org, 29 Apr. 2021, arxiv.org/abs/2010.01412. '''
+
+class SAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+
+        defaults = dict(rho=rho, **kwargs)
+        super(SAM, self).__init__(params, defaults)
+
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None: continue
+                e_w = p.grad * scale.to(p)
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+                self.state[p]["e_w"] = e_w
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None: continue
+                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
+        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+                    torch.stack([
+                        p.grad.norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                    p=2
+               )
+        return norm
+
+
 if __name__ == "__main__":
-    train_loader, test_loader, noise_loader = create_dataloaders() 
+    train_loader, test_loader, noise_loader = create_dataloaders()
